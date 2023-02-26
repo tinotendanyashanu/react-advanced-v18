11517bc6685873e0aaf0faabbf73b85d8d8fd183
Tinotenda Nyashanau <tinotendanyash@gmail.com>
2023-02-26 20:45:51 +0100

    used state objects

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/1-useState/setup/2-useState-basics";
+import Setup from "./tutorial/1-useState/setup/4-useState-object";
 function App() {
   return (
     <div className="container">
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    Name: "Tinotenda",
+    Surename: " Nyashanu",
+    Title: "Mr",
+  });
+  const onclickevent = () => {
+    if (person.Title === "Mr") {
+      setPerson({ ...person, Title: "mrs" });
+    } else {
+      setPerson({ ...person });
+    }
+  };
+  return (
+    <>
+      <h3>{person.Name}</h3>
+      <h3>{person.Surename}</h3>
+      <h3>{person.Title}</h3>
+
+      <button className="btn" onClick={onclickevent}>
+        Click to make changes{" "}
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
